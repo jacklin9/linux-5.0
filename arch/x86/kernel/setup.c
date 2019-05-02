b954208f9d721feac7ebfeeb5785d08732d1f1c7
Jack Lin <jacklin_9@msn.com>
2019-05-02 10:57:30 +0800

    20190502

--- arch/x86/kernel/setup.c
+++ arch/x86/kernel/setup.c
@@ -821,7 +821,7 @@ dump_kernel_offset(struct notifier_block *self, unsigned long v, void *p)
 void __init setup_arch(char **cmdline_p)
 {
 	memblock_reserve(__pa_symbol(_text),
-			 (unsigned long)__bss_stop - (unsigned long)_text);	/// Add range [_text, __bss_stop] to memblock.reserved
+			 (unsigned long)__bss_stop - (unsigned long)_text);	/// Add range phyaddr([_text, __bss_stop]) to memblock.reserved
 
 	/*
 	 * Make sure page 0 is always reserved because on systems with
